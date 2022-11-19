c5fbb9448b65d1ee74ff425c976ffc4df12536cc
Mr-i-me <Mr-i-me@Ivans-MacBook-Pro.local>
2022-11-19 18:27:44 -0300

    fix: layers

--- src/handlers/CreateCard/CreateCard.js
+++ src/handlers/CreateCard/CreateCard.js
@@ -1,47 +1,30 @@
-const path = process.env.NODE_ENV === 'test' ? '../../../layers/common/' : '/opt/';
+const {PipefyService} = require(`opt/services/Pipefy.service`);
 
-const { PipefyService} = require(`${path}Library/Application Support/JetBrains/WebStorm2022.2/scratches/pipefy/service`);
-const {cards: {showCard}} = require(`${path}Library/Application Support/JetBrains/WebStorm2022.2/scratches/pipefy/queries`);
-const {createTableRecord: createTableRecordMutations} = require(`${path}Library/Application Support/JetBrains/WebStorm2022.2/scratches/pipefy/mutations`);
-const  Response =(status=200, data)=>( {
+const {cards: {showCard}} = require(`opt/pipefy/queries`);
+// const {createTableRecord: createTableRecordMutations} = require(`${path}/pipefy/mutations`);
+const Response = (status = 200, data) => ({
     statusCode: status,
     body: JSON.stringify(data),
 });
-const createTableRecords = async ({pipefy}) => {
-    const records = [];
 
-    for (const call of calls) {
-        const fields = createPayload(data);
-
-        const recordData = await pipefy.gqlRequest({
-            document: createTableRecordMutations,
-            variables: {tableId: TABLE_ID, fieldsAttributes: fields.filter(attr => attr.field_value !== '')}
-        });
-
-        const {createTableRecord: {table_record: {id: recordId}}} = recordData;
-        records.push(recordId)
-    }
-    return records
-}
-
-
-const API_KEY = '01011001'
 const CrateCardHandler = async (event) => {
+    const {env: {API_KEY}} = process;
     const {body: {data: {body}}} = event;
-    const {data: {card: {id:cardId}}} = body;
-    JSON.parse(body)
-    /*
-    * @Data
-    * number_of_call
-    * cardData
-    * */
+    const {
+        data: {card: {id: cardId}}
+    } = JSON.parse(body);
+    console.log({cardId})
 
-    const number_of_call = 1
+
+    const number_of_call = 1;
     const calls = [...Array(number_of_call)].map((x, index) => index);
     const pipefy = new PipefyService(API_KEY);
-    const cardData = await pipefy.gqlRequest({document: showCard, variables: {cardId}});
-    console.log({cardData})
+    const cardData = await pipefy.gqlRequest({
+        document: showCard,
+        variables: {cardId},
+    });
+    console.log({cardData});
 
-    return Response(200, {"ok": records});
+    return Response(200, {ok: "OK"});
 };
-module.exports = {CrateCardHandler}
+module.exports = {CrateCardHandler};
--- src/handlers/CreateCard/helpers.js
+++ src/handlers/CreateCard/helpers.js
@@ -1,44 +1,44 @@
-const path = process.env.NODE_ENV === 'test' ? '../../../layers/common/' : '/opt/';
-const {PipefyService} = require(`${path}/pipefy/service`);
-
-const {listPipes, cards: {showCard}} = require(`${path}pipefy/queries`);
-const {createTableRecord: createTableRecordMutations} = require(`${path}pipefy/mutations`);
-
-const datePlusIndex = (startDate, index) => {
-    const date = new Date(startDate)
-    date.setDate(date.getDay() + index)
-    return date
-}
-
-const createPayload = (data = {}, index) => {
-    // todo add +1 to each date
-    const {start_date: startDate, end_date: endDate} = data;
-    const NewEndDate = datePlusIndex(endDate, index)
-    const newStartDate = datePlusIndex(startDate, index)
-    return [
-        {field_id: "interval_index", field_value: index + 1},
-        {field_id: "start_date", field_value: newStartDate},
-        {field_id: "end_date", field_value: NewEndDate},
-        // todo shifts
-        // {field_id: "shifts", field_value: "shifts"}
-    ];
-};
-
-
-// creates a table recortd and returns its id
-const createTableRecord = async (data, daysToAdd) => {
-    const pipefy = new PipefyService(API_KEY);
-    const fields = createPayload(data);
-
-    const recordData = await pipefy.gqlRequest({
-        document: createTableRecordMutations,
-        variables: {tableId: TABLE_ID, fieldsAttributes: fields.filter(attr => attr.field_value !== '')}
-    });
-
-    const {
-        createTableRecord: {
-            table_record: {id: recordId}
-        }
-    } = recordData;
-    return recordId
-}
+// const path = process.env.NODE_ENV === 'test' ? '../../../layers/common/' : '/opt/';
+// // const {PipefyService} = require(`${path}/pipefy/service`);
+//
+// const {listPipes, cards: {showCard}} = require(`${path}pipefy/queries`);
+// const {createTableRecord: createTableRecordMutations} = require(`${path}pipefy/mutations`);
+//
+// const datePlusIndex = (startDate, index) => {
+//     const date = new Date(startDate)
+//     date.setDate(date.getDay() + index)
+//     return date
+// }
+//
+// const createPayload = (data = {}, index) => {
+//     // todo add +1 to each date
+//     const {start_date: startDate, end_date: endDate} = data;
+//     const NewEndDate = datePlusIndex(endDate, index)
+//     const newStartDate = datePlusIndex(startDate, index)
+//     return [
+//         {field_id: "interval_index", field_value: index + 1},
+//         {field_id: "start_date", field_value: newStartDate},
+//         {field_id: "end_date", field_value: NewEndDate},
+//         // todo shifts
+//         // {field_id: "shifts", field_value: "shifts"}
+//     ];
+// };
+//
+//
+// // creates a table recortd and returns its id
+// // const createTableRecord = async (data, daysToAdd,API_KEY) => {
+// //     const pipefy = new PipefyService(API_KEY);
+// //     const fields = createPayload(data);
+//
+// //     const recordData = await pipefy.gqlRequest({
+// //         document: createTableRecordMutations,
+// //         variables: {tableId: TABLE_ID, fieldsAttributes: fields.filter(attr => attr.field_value !== '')}
+// //     });
+//
+// //     const {
+// //         createTableRecord: {
+// //             table_record: {id: recordId}
+// //         }
+// //     } = recordData;
+// //     return recordId
+// // }
